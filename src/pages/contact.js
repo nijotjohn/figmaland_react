56d8e355e005a862f96c36d8167be007b797c1e8
Nijo John <nijo.john@eqs.com>
2021-08-29 13:30:32 +0530

    pages

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+  return (
+    <main>
+      <title>Contact</title>
+      <h1>Contact</h1>
+    </main>
+  )
+}
+
+export default AboutPage
---
+++ src/pages/product.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+  return (
+    <main>
+      <title>Product</title>
+      <h1>Product</h1>
+    </main>
+  )
+}
+
+export default AboutPage
